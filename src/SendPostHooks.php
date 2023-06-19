57bcb521bfea81b1be45c81378a0a1ee98c9108d
rafaell1995 <rafaeldearaujop95@gmail.com>
2023-06-19 10:40:10 -0300

    Created methods for send to zapier and manager columns posts

--- src/SendPostHooks.php
+++ src/SendPostHooks.php
@@ -21,7 +21,10 @@ class SendPostHooks
 
     public function initHooks()
     {
-        
+        add_filter('manage_posts_columns', array( $this, 'social_post_column' ));
+        add_action('manage_posts_custom_column', array( $this, 'social_post_column_content' ), 10, 2);
+        add_action('admin_enqueue_scripts', array( $this, 'enqueue_social_scripts' ));
+        add_action('wp_ajax_send_to_social', array( $this, 'send_to_social' ));
     }
     
     /**
@@ -50,8 +53,6 @@ class SendPostHooks
             'featured_image_url' => $post_featured_image_url
         );
 
-        // webhook url
-        $url = 'https://hooks.zapier.com/hooks/catch/15470395/3hkw1xn/';
         $args = array(
             'method' => 'POST',
             'headers' => array(
@@ -60,8 +61,74 @@ class SendPostHooks
             'body' => json_encode($data)
         );
 
-        // sending data to webhook
-        $response = wp_remote_post($url, $args);
+        if( ! empty( get_option( 'send_post_social_network_options' ) ) && isset( get_option( 'send_post_social_network_options' )['zapier_endpoint_url'] ) )
+        {
+            // webhook url
+            $url = get_option( 'send_post_social_network_options' )['zapier_endpoint_url'];
+
+            // sending data to webhook
+            $response = wp_remote_post($url, $args);
+            
+            // Check if the request was successful
+            if( is_wp_error( $response ) ) {
+                wp_send_json_error( 'There was an error making the request.' );
+                die();
+            } else {
+                $body = wp_remote_retrieve_body( $response );
+                $data = json_decode( $body );
+
+                // Após enviar para o Zapier, atualize o meta_value
+                update_post_meta($post_id, 'social_published', true);
+
+                wp_send_json_success( $data );
+            }
+        } else {
+            wp_send_json_error( 'Not endpoint url in settings.' );
+            die();
+        }
         
+    }
+
+    // Adicionar coluna personalizada na listagem dos posts
+    public function social_post_column($columns) {
+        $columns['social'] = 'Social';
+        return $columns;
+    }
+
+    // Exibir conteúdo da coluna personalizada
+    public function social_post_column_content($column_name, $post_id) {
+        if ($column_name == 'social') {
+            $is_published = get_post_meta($post_id, 'social_published', true);
+            $post_status = get_post_status($post_id);
+            if( $post_status == 'publish' )
+            {
+                if ($is_published) {
+                    echo 'Enviado para Redes Sociais';
+                } else {
+                    echo '<button class="social-button" data-postid="' . $post_id . '">Enviar para Redes Sociais</button>';
+                }
+            } else {
+                echo 'O post tem que estar publicado';
+            }
+        }
+    }
+
+    // Adicionar scripts e estilos para a funcionalidade do botão
+    public function enqueue_social_scripts() {
+        wp_enqueue_script('jquery');
+        wp_enqueue_script('social-script', SEND_POST_SOCIAL_NETWORK_URL . '/assets/js/send-to-social.js', array('jquery'), '1.0', true);
+        wp_localize_script('social-script', 'custom_vars', array(
+            'ajax_url' => admin_url('admin-ajax.php')
+        ));
+    }
+
+    // Manipular a requisição AJAX para enviar dados para o Zapier e atualizar o meta_value
+    public function send_to_social() {
+        if (isset($_POST['post_id'])) {
+            $post_id = intval($_POST['post_id']);
+            // Aqui você deve chamar sua função de envio da requisição para o Zapier
+            $this->send_post_data_to_webhook($post_id);
+        }
+        wp_die();
     }
 }
